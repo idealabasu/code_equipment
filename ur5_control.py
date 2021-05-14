f74e2ea72b67c8d808d7fc119e8c126bf7579795
dli-sys <38682277+dli-sys@users.noreply.github.com>
2021-05-14 14:50:47 -0700

    Update ur5_control.py

--- ur5_control.py
+++ ur5_control.py
@@ -2,59 +2,53 @@ import numpy as np
 import urx
 import time
 
+import numpy as np
+
 def Init_ur5(ur5_port):
     try:
         tcp = ((0,0,0,0,0,0))
         payload_m = 1
         payload_location = (0,0,0.5)
-        global ur5
-        import urx    
+        import urx      
         ur5 = urx.Robot(ur5_port)
         ur5.set_tcp(tcp)
         ur5.set_payload(payload_m, payload_location)
-        try:
-            rospy.loginfo("UR5 connected")
-        except:
-            print("UR5 Connected")
     except:
-        try:
-            rospy.loginfo("Could not connect, check connection")
-        except:
-            print("Could not connect, check connection")
+            print("Can not connect, check connection and try again")    
+    if ur5 == None:
+        print("Can not connect, check connection and try again")
+    elif ur5.host == ur5_port:
+        print("UR5 connected at: " + ur5_port)
+    else:
+        print("Can not connect, check connection and try again")    
+    return ur5
+    
 
-
-def move_ur5(moving_vector,v,a):
+def move_ur5(ur5,moving_vector,v,a,wait=False):
     current_pose = ur5.get_pose()
     current_pose.pos[:] += moving_vector
-    ur5.set_pose(current_pose,vel = v,acc = a)    
+    ur5.movel(current_pose,vel=v,acc=a,wait=wait)
 
-def move_ur5_wait(moving_vector,v,a):
-    current_pose = ur5.get_pose()
-    current_pose.pos[:] += moving_vector
-    ur5.set_pose(current_pose,vel = v,acc = a)    
-
-
-def rotating_ur5_z(angle, v, a,flag):
+def rotating_ur5_z(ur5,angle, v, a,wait=False):
+    #in degreee
+    import math
     current_pose = ur5.get_pose()
     v*=0.00454319282637702
-#    ur5.set_pose(current_pose,vel = v,acc = a)
-    rad = rad = math.radians(angle)
+    rad  = math.radians(angle)
     current_pose.orient.rotate_zt(rad)
-#    new_pose = current_pose
-    ur5.set_pose(current_pose,vel = v,acc = a,wait=flag)
+    ur5.set_pose(current_pose,vel = v,acc = a,wait=wait)
+  
+# for ur5e
+moving_vector_left = np.array((0.01,0,0))
+moving_vector_right = np.array((-0.01,0,0))
+moving_vector_forward = np.array((0,-0.01,0))
+moving_vector_backward = np.array((0,0.01,0))
+moving_vector_up = np.array((0,0,0.01))
+moving_vector_down = np.array((0,0,-0.01))
 
-ur5_port = '192.168.0.102'
-vel = 1/100
-acc = 1
+ur5e = Init_ur5("192.168.1.103")
 
-Init_ur5()
+loc=ur5e.get_pose()
 
-loc=ur5.get_pose()
-loc_0 = ur5.get_pos()
-
-
-
-move_ur5(moving_vector_up*distance,vel,acc)
-
-
-ur5.set_pose(loc_0,vel,acc,wait=True)
+move_ur5(ur5e,moving_vector_up,0.01,1,wait=False)
+rotating_ur5_z(ur5e,10,1,wait=False)
