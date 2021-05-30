f519039312c59ae49ba6169ace5997a548a4bd6c
dli-sys <38682277+dli-sys@users.noreply.github.com>
2021-05-29 21:05:32 -0700

    Update control_thorlab_linear_stage.py

--- control_thorlab_linear_stage.py
+++ control_thorlab_linear_stage.py
@@ -1,3 +1,8 @@
+"""
+configuring this device is a little bit tricky
+Please refer to: https://github.com/qpit/thorlabs_apt to install the library and copy the .dll fils
+"""
+
 import thorlabs_apt as apt
 
 def init_thorlabs(blocking=True):
@@ -10,14 +15,19 @@ def init_thorlabs(blocking=True):
     
     return motor
 
-
-
+# Here we initialize the device and move the linear stage to its home position
 motor = init_thorlabs(blocking=True)
 motor.move_home(True)
 
+# Here is to initialize the parameters
 motor.set_move_home_parameters(*motor.get_move_home_parameters())
 motor.set_velocity_parameters(*motor.get_velocity_parameters())
 
-
+# Here we set the min_vel, acc and max_vel of the linear stage
+# Then we move the linear stage to 145mm
 motor.set_velocity_parameters(0,1000,10)
-motor.move_to(145,blocking=True)
+motor.move_to(145,blocking=False)
+
+# Here  we move the linear stage by 10mm
+motor.set_velocity_parameters(0,1000,10)
+motor.move_by(10,blocking=False)
