0618de360e42c135b01f7c99efc3de5b074c12ed
dli-sys <38682277+dli-sys@users.noreply.github.com>
2022-07-08 13:41:47 -0700

    Update control_dynamixel_servo.py

--- python/idealab_equipment/control_dynamixel_servo.py
+++ python/idealab_equipment/control_dynamixel_servo.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 31 18:20:27 2021
-
 @author: dongting
 """
 
@@ -15,7 +14,6 @@ import time
 
 """
 Dynamixel Initialaztion
-
 """
 
 # if os.name == 'nt':
@@ -40,6 +38,7 @@ from dynamixel_sdk import *
 # Control table address
 ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
 ADDR_PRO_GOAL_POSITION      = 116
+ADDR_PRO_GOAL_CURRENT      = 102
 ADDR_PRO_PRESENT_POSITION   = 132
 ADDR_PRO_PRESENT_VELOCITY   = 128
 ADDR_PRO_PRESENT_CURRENT   = 126
@@ -58,11 +57,11 @@ DXL_OPERATING_is_PWM = 3
 PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-DXL_ID_1                     = 0             # Dynamixel ID : 1
+DXL_ID_1                     = 1             # Dynamixel ID : 1
 
 BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
 #DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-DEVICENAME                  = 'COM4'    # Check which port is being used on your controller
+DEVICENAME                  = 'COM8'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
 TORQUE_ENABLE               = 1                 # Value for enabling the torque
@@ -98,6 +97,7 @@ else:
 
 # Enable Dynamixel Torque
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+
 if dxl_comm_result != COMM_SUCCESS:
     print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 elif dxl_error != 0:
@@ -106,36 +106,42 @@ else:
     print("Dynamixel_1 has been successfully connected")
     
 
-# Position Control
-dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_OPERATING_MODE,DXL_OPERATING_is_POSITION)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, 65,1)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, 65,0)
 
-dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+# Disable torque control
 # dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
 
+# # Position Control
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_OPERATING_MODE,DXL_OPERATING_is_POSITION)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
 
-DXL_PROFILE_SPEED = 200
-DXL_GOAL_POSITION = 1500
+# DXL_PROFILE_SPEED = 200
+# DXL_GOAL_POSITION = 1000
 
-dxl_comm_result_1, dxl_error_1 = packetHandler.write4ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_PRPFILE_VELOCITY,DXL_PROFILE_SPEED)
-dxl_comm_result_1, dxl_error_1 = packetHandler.write4ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_GOAL_POSITION, DXL_GOAL_POSITION)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write4ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_PRPFILE_VELOCITY,DXL_PROFILE_SPEED)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write4ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_GOAL_POSITION, DXL_GOAL_POSITION)
+# END Position Control
+
+# current control
+dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_OPERATING_MODE,DXL_OPERATING_is_CURRENT)
+dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
+DXL_GOAL_CURRENT = 0
+dxl_comm_result_1, dxl_error_1 = packetHandler.write2ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_GOAL_CURRENT, DXL_GOAL_CURRENT)
+# END current control
 
 
 
 # Velocity Control
-# Still developing this part......
-dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_OPERATING_MODE,DXL_OPERATING_is_VELOCITY)
 
-dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE)
+# DXL_GOAL_VELOCITY = 0
+# dxl_comm_result_1, dxl_error_1 = packetHandler.write4ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_GOAL_VELOCITY, DXL_GOAL_VELOCITY)
 
-dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, ADDR_PRO_OPERATING_MODE,DXL_OPERATING_is_VELOCITY)
+# End Velocity control
 
+# CLOSE servo port
+portHandler.closePort()
 
-# # dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, 98,0)
-
-# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, 44, 255)
-
-# # dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1, 112, 4720)
-
-# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1,104,100)
-
-# dxl_comm_result_1, dxl_error_1 = packetHandler.write1ByteTxRx(portHandler, DXL_ID_1,65,1)
